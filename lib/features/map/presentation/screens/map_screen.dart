5b5c27e218f93b2160af087b3e047df8ca4cc844
박신영 <park_new0@naver.com>
2025-09-13 14:21:33 +0900

    feat(map): 지도 화면 기본 기능 구현

--- lib/features/map/presentation/screens/map_screen.dart
+++ lib/features/map/presentation/screens/map_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mongle_flutter/features/map/presentation/viewmodels/map_viewmodel.dart';
+import 'package:mongle_flutter/features/map/presentation/widgets/map_view.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends ConsumerWidget {
   const MapScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('지도 화면')));
+  Widget build(BuildContext context, WidgetRef ref) {
+    // 2. ref.watch를 사용하여 mapViewModelProvider의 상태 변화를 감지합니다.
+    //    상태가 바뀔 때마다 이 build 메소드는 자동으로 다시 실행됩니다.
+    final mapState = ref.watch(mapViewModelProvider);
+
+    // 3. freezad 덕분에 when 메소드를 사용하여 모든 상태를 안전하게 처리할 수 있습니다.
+    return mapState.when(
+      // 로딩 중일 때 보여줄 UI
+      loading: () => const Center(child: CircularProgressIndicator()),
+      // 에러 발생 시 보여줄 UI
+      error: (message) => Center(child: Text(message)),
+      // 데이터 로딩 성공 시 보여줄 UI
+      data: (initialPosition) => MapView(initialPosition: initialPosition),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mongle_flutter/core/navigation/router.dart'; // 방금 만든 라우터 import
+import 'package:mongle_flutter/core/navigation/router.dart';
 
-void main() {
+void main() async {
   // TODO: 추후 Firebase, Naver Map 등 비동기 초기화 로직 추가 예정
+  // 1. runApp() 전에 Flutter 엔진과 위젯 바인딩이 준비되도록 보장합니다.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. .env 파일을 로드합니다.
+  await dotenv.load(fileName: ".env");
+  final naverMapClientId = dotenv.env['NAVER_MAP_CLIENT_ID'];
+
+  // 3. Naver Map SDK를 초기화합니다. Client ID는 .env 파일에서 안전하게 가져옵니다.
+  await FlutterNaverMap().init(
+    clientId: naverMapClientId!,
+    onAuthFailed: (ex) {
+      switch (ex) {
+        case NQuotaExceededException(:final message):
+          print("사용량 초과 (message: $message)");
+          break;
+        case NUnauthorizedClientException() ||
+            NClientUnspecifiedException() ||
+            NAnotherAuthFailedException():
+          print("인증 실패: $ex");
+          break;
+      }
+    },
+  );
 
   runApp(
     // Riverpod를 앱 전체에서 사용하기 위해 ProviderScope로 감싸줍니다.
