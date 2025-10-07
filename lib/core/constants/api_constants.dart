cdc7fe467489ed8ec426f19bbc54f1769f6c7271
박신영 <park_new0@naver.com>
2025-10-08 07:44:40 +0900

    feat(singup): apiconstants 파일에 인증 코드 api 경로 추가

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,16 +2,17 @@ class ApiConstants {
   // private 생성자로 외부에서 인스턴스 생성을 막습니다.
   ApiConstants._();
 
-  static const String login = '/auth/login';
   static const String signUp = '/auth/sign-up';
+  static const String verificationCode = '/auth/verification-code';
+
+  static const String login = '/auth/login';
   static const String logout = '/auth/logout';
   static const String reissue = '/auth/reissue';
 
-  static const String posts = '/posts';
   static const String postFileUploadUrls = '/post-files/upload-urls';
+  static const String posts = '/posts';
 
   static String postById(String postId) => '/posts/$postId';
-
   static String getComments(String postId) => '/posts/$postId/comments';
   static String getReplies(String parentCommentId) =>
       '/comments/$parentCommentId/replies';
